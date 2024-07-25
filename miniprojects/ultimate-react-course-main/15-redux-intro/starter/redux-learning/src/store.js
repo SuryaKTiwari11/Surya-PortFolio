7a233488e30d7ebc0906e76cf89eb0f61f4634f9
SuryaKantTiwari <jamcocobutter@gmail.com>
2024-07-26 02:27:33 +0530

    updates

--- miniprojects/ultimate-react-course-main/15-redux-intro/starter/redux-learning/src/store.js
+++ miniprojects/ultimate-react-course-main/15-redux-intro/starter/redux-learning/src/store.js
@@ -1,10 +1,15 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+  fullName: " ",
+  nationalID: " ",
+  createAt: " ",
+};
+function accountReducer(state = initialState, action) {
   switch (action.type) {
     case "account/deposit":
       return { ...state, balance: state.balance + action.payload };
@@ -13,9 +18,13 @@ function reducer(state = initialState, action) {
 
     case "account/requestLoan":
       if (state.loan > 0) return state;
-      //todoLATER
-      return { ...state, loan: action.payload };
-    case "acount/payLoan":
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
       return {
         ...state,
         loan: 0,
@@ -27,5 +36,51 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
-const store = createStore(reducer);
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.payload) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createAt: action.payLoan.createAt,
+      };
+    case "customer/upDateName":
+      return { ...state, fullName: action.payload.fullName };
+    default:
+      return state;
+  }
+}
+const rootReducer = combineReducers({
+  account:accountReducer,
+  customer:customerReducer,
+})
+const store = createStore(accountReducer);
 
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+function payLoan(amount, purpose) {
+  return {
+    type: "account/payLoan",
+    payload: { amount, purpose },
+  };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(200));
+
+function CreateCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: fullName,
+    nationalID,
+    createAt: new Date().toISOString(),
+  };
+}
+function UpdateName(fullName) {
+  return { type: "account/updateName", payload: fullName };
+}
